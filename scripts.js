27c44959d05d41c08a6f3c515ec735f8019ff841
Anas-bouzbouz <anas-bouzbouz@hotmail.com>
2025-07-31 22:37:34 +0100

    done

--- scripts.js
+++ scripts.js
@@ -7,11 +7,10 @@ subscriptionBtn.addEventListener('click', function(event) {
 
     if (dropdownMenu.style.display === 'none') {
         dropdownMenu.style.display = 'block' ; 
-        
-        arrow[0].style.transform = "scaleY(-1)"; // vertical flip
+        arrow[0].style.transform = "rotate(-180deg)"; // flip the arrow vertically
+
     }else {
         dropdownMenu.style.display = 'none';
-        
-        arrow[0].style.transform = "scaleY(1)"; // remove vertical flip
+        arrow[0].style.transform = "rotate(0deg)"; // remove vertical flip
     }
 });
